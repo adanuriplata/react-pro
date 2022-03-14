8808ba49ad018c1860945afaf44403f70f79f217
distordev <adanuriplata@gmail.com>
2022-03-13 20:52:22 -0600

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,16 +1,23 @@
-import React, { createContext } from 'react'
+import React, { createContext, ReactElement } from 'react'
 import styles from '../styles/styles.module.css'
 
 import { useProduct } from '../hooks/useProduct'
-import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+import { Product, ProductContextProps } from '../interfaces/interfaces';
 
 
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
+export interface Props {
+    product: Product;
+    children?: ReactElement | ReactElement[]
+    className?: string;
+    style?: React.CSSProperties
+}
 
-export const ProductCard = ({children, product }: ProductCardProps ) => {
+
+export const ProductCard = ({children, product, className, style }: Props ) => {
 
     const {counter, increaseBy} = useProduct()
 
@@ -21,7 +28,8 @@ export const ProductCard = ({children, product }: ProductCardProps ) => {
           product,
       }}>
 
-    <div className={ styles.productCard }>
+    <div className={ `${styles.productCard} ${ className }` }
+        style={style} >
         {children}
     </div>
 </Provider>
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,5 @@
 import { ProductImage, ProductTitle, ProductButtons, ProductCard } from '../components';
+import '../styles/custom-styles.css'
 
 const product = {
     id: '1',
@@ -17,17 +18,26 @@ const ShoppingPage = () => {
             flexDirection: 'row',
             flexWrap: 'wrap'
         }}>
-            <ProductCard product={product} >
-                <ProductCard.Image />
-                <ProductCard.Title/>
-                <ProductCard.Buttons />
+            <ProductCard product={product} className="bg-dark text-white" >
+                <ProductCard.Image className="custom-image" />
+                <ProductCard.Title className="text-bold" />
+                <ProductCard.Buttons className='custom-buttons'/>
 
             </ProductCard>
 
-            <ProductCard product={product} >
-                <ProductImage />
-                <ProductTitle  title={ 'Hola Mundo' }/>
-                <ProductButtons />
+            <ProductCard className="bg-dark text-white" product={product} >
+                <ProductImage className="custom-image" />
+                <ProductTitle  className="text-bold"/>
+                <ProductButtons className='custom-buttons' />
+
+            </ProductCard>
+
+            <ProductCard className="bg-dark text-white" product={product} 
+                style={{ backgroundColor: '#70d1f8'  }}
+            >
+                <ProductImage style={{ background: '#fff' }}  />
+                <ProductTitle style={{ font: 'flex' }}  />
+                <ProductButtons style={{ display: 'flex', justifyContent:'end' }}/>
 
             </ProductCard>
 
